af27f1e0d3c969c058abc4fa6a7e53170e86fc12
QuentinHsu <xuquentinyang@gmail.com>
2025-04-09 21:51:56 +0800

    fix: correct log statement format for supported languages

--- src/extension.ts
+++ src/extension.ts
@@ -25,7 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
 			case 'astro':
 			case 'svelte':
 			case 'html':
-				logStatement = `${indentation}console.log({${selectedText}});\n`;
+				logStatement = `${indentation}console.log('${selectedText}', ${selectedText});\n`;
 				break;
 			case 'go':
 				logStatement = `${indentation}log.Printf("%v: %+v\\n", "${selectedText}", ${selectedText})\n`;
